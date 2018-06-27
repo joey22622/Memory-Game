9a5ef9af34346b1a27e79ff56ecfff640c432a5e
Joey <jdechant970@gmail.com>
2018-06-26 18:15:24 -0600

    building game start component, left off inside rendering new array for cardArr state

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './components/Card';
+import GameStart from './components/GameStart';
+import API from './utils/API';
+
+
+class App extends Component {
+  state = {
+    cards: [],
+    query: ""
+  };
+
+componentDidMount(){
+
+}
+loadGame(query){
+    API.getGifs("dogs")
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+
+
+
+  render() {
+    return (
+      <div className="content-wrap">
+        <GameStart/>
+        <h2>dummy text</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
