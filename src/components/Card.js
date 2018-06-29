c8679382e8a2126c1b4b71d23c862d70c6c297cb
Joey <jdechant970@gmail.com>
2018-06-29 09:36:50 -0600

    Updates

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = props => {
-    return (<div key={props.id} className="card" onClick={props.selectFunction}>
-        <img alt={props.title} src={props.url}/>
-    </div>)
-}
-
-export default Card; 
