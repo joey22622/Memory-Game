c5c3fa8e4eca8de22fb3cc081fcfef793b00db83
Joey <jdechant970@gmail.com>
2018-06-29 18:20:26 -0600

    Updates

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = props => {
-    return (<div key={props.keyId} data-guessed={props.guessed} className="card" onClick={() => props.clicked(props.id, props.guessed)}>
-        <img alt={props.title} src={props.url}/>
-    </div>)
-}
-
-export default Card; 
--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-
-export default {
-    getGifs: function(query) {
-        return axios.get(`https://api.giphy.com/v1/gifs/search?apiKey=ih2IbJeuF1j03d7wJ5MpKRi13l3cXBAp&limit=12&q=${query}`)
-    }
-}
